fbc20a5e88d4d59c33b9db43a45ba69c109e9921
Shuaib2698 <shuaib9845@gmail.com>
2025-09-01 12:07:59 +0530

    updated

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,7 @@ const upload = require('../middleware/uploadMiddleware');
 // Define routes
 router.get('/', protect, admin, userController.getAllUsers);
 router.get('/count', protect, admin, userController.getUsersCount);
+router.get('/me', protect, userController.getCurrentUser);
 router.get('/:id', protect, userController.getUserById);
 router.get('/me', protect, userController.getCurrentUser);
 router.put('/profile', protect, userController.updateProfile);
